d4f18dd51d1801f1236ffdada4d557a4499bcc78
jiro4989 <jiroron666@gmail.com>
2020-01-10 23:14:36 +0900

    プレフィックスでジャンプできるようにした

--- src/dirsel.nim
+++ src/dirsel.nim
@@ -1,4 +1,4 @@
-import os, strutils, tables
+import os, strutils, tables, algorithm
 export tables
 
 import illwill
@@ -15,31 +15,22 @@ type
   GroupedFiles* = OrderedTable[string, seq[string]]
   Terminal = ref object
     tb: TerminalBuffer
-    x, y: int
+    selectedItemIndex: int
+    cwd: string
+    files: seq[string]
 
-proc listFilesGroupByPrefix*(dir: string): GroupedFiles =
-  ## ファイル名のプレフィックスでグルーピング。
-  ## 半角文字のみグルーピング。
-  ## マルチバイト文字はその他扱い。
-  result = initOrderedTable[string, seq[string]]()
-  result["~"] = @[]
-  for k, p in walkDir(dir):
-    let
-      base = lastPathPart(p)
-      prefix = base[0].toLowerAscii
-      prefixStr = $prefix
-    if prefix.isAlphaAscii or prefix == '.':
-      if not result.hasKey(prefixStr):
-        result[prefixStr] = @[]
-      result[prefixStr].add(base)
-    else:
-      result["~"].add(base)
-  proc cmp(x, y: (string, seq[string])): int =
-    if x[0] < y[0]: 0
-    else: 1
-  result.sort(cmp)
+proc setCurrentFiles(term: var Terminal) =
+  var files: seq[string]
+  for kind, path in walkDir(term.cwd):
+    let base = path.lastPathPart()
+    files.add(base)
+  files.sort()
+  term.files = files
 
-proc selectElement*() = discard
+proc newTerminal(): Terminal =
+  result = Terminal()
+  result.cwd = getCurrentDir()
+  result.setCurrentFiles()
 
 proc exitProc() {.noconv.} =
   ## 終了処理
@@ -54,23 +45,30 @@ proc exitProc() {.noconv.} =
   echo output
   quit(0)
 
-proc fileColor(kind: PathComponent): ForegroundColor =
-  if kind == pcDir:
-    fgBlue
-  else:
-    fgWhite
+proc searchPrefix(term: var Terminal, prefix: char) =
+  let
+    idx = term.selectedItemIndex
+    files = term.files
+  if idx + 1 < files.len and files[idx][0] == prefix and files[idx + 1][0] == prefix:
+    inc(term.selectedItemIndex)
+    return
+
+  for i, file in files:
+    if file.startsWith(prefix):
+      term.selectedItemIndex = i
+      break
 
 proc redraw(term: Terminal) =
-  let cwd = getCurrentDir()
-  let files = listFilesGroupByPrefix(cwd)
-  var y: int
-  for key, paths in files.pairs:
-    if term.y == y:
+  let cwd = term.cwd
+  term.tb.write(0, 0, cwd)
+  var y = 0
+  for path in term.files:
+    if term.selectedItemIndex == y:
       term.tb.setBackgroundColor(bgGreen)
-    let line = $key & ": " & paths.join(" ")
-    term.tb.write(0, y, line)
-    term.tb.resetAttributes()
+      output = cwd / path
+    term.tb.write(0, y+1, path)
     inc(y)
+    term.tb.resetAttributes()
 
 proc downDir(itemIndex: var int) =
   let cwd = getCurrentDir()
@@ -91,7 +89,7 @@ proc main =
   setControlCHook(exitProc)
   hideCursor()
 
-  var term = new Terminal
+  var term = newTerminal()
   while true:
     # 後から端末の幅が変わる場合があるため
     # 端末の幅情報はループの都度取得
@@ -107,27 +105,43 @@ proc main =
     var key = getKey()
     case key
     of Key.None: discard
-    of Key.Escape, Key.Q: exitProc()
+    of Key.Escape: exitProc()
+    of Key.A,
+       Key.B,
+       Key.C,
+       Key.D,
+       Key.E,
+       Key.F,
+       Key.G,
+       Key.I,
+       Key.M,
+       Key.N,
+       Key.O,
+       Key.P,
+       Key.Q,
+       Key.R,
+       Key.S,
+       Key.T,
+       Key.U,
+       Key.V,
+       Key.W,
+       Key.X,
+       Key.Y,
+       Key.Z:
+      let key = ($key)[0].toLowerAscii
+      term.searchPrefix(key)
     of Key.J:
-      inc(term.y)
+      inc(term.selectedItemIndex)
     of Key.K:
-      dec(term.y)
-      if term.y < 0:
-        term.y = 0
+      dec(term.selectedItemIndex)
+      if term.selectedItemIndex < 0:
+        term.selectedItemIndex = 0
     of Key.H:
-      term.y = 0
-      let cwd = getCurrentDir()
-      setCurrentDir(cwd.parentDir())
-    of Key.L:
-      discard
-    of Key.Space:
-      discard
-    of Key.C:
-      discard
+      term.selectedItemIndex = 0
+      term.cwd = term.cwd.parentDir()
+      term.setCurrentFiles()
     of Key.Enter:
       exitProc()
-    of Key.S:
-      searchQuery = "j"
     else: discard
 
     term.tb.display()
